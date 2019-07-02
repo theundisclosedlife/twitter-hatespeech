f07f7311d2b404fb8319e72d295ccf29a2469416
theundisclosedlife <31027232+theundisclosedlife@users.noreply.github.com>
2019-07-02 13:46:00 +0530

    changed data handler

--- data_handler.py
+++ data_handler.py
@@ -2,21 +2,20 @@ import json
 import pdb
 import codecs
 import pdb
+import pandas as pd
 
 def get_data():
     tweets = []
-    files = ['racism.json', 'neither.json', 'sexism.json']
-    for file in files:
-        with codecs.open('./tweet_data/' + file, 'r', encoding='utf-8') as f:
-            data = f.readlines()
-        for line in data:
-            tweet_full = json.loads(line)
-            tweets.append({
-                'id': tweet_full['id'],
-                'text': tweet_full['text'].lower(),
-                'label': tweet_full['Annotation'],
-                'name': tweet_full['user']['name'].split()[0]
-                })
+    df=pd.read_csv("Untitled.csv")
+    df = df.drop(["Unnamed: 0"], axis=1)
+    df['polarity'] = (df['Code']=="H").astype(int)
+   
+    for i in range(0,len(df['Tweet']):
+        tweets.append({'text' : df['Tweet'][i].lower(),
+                       'label': df['polarity'][i]
+                      })
+        
+               
 
     #pdb.set_trace()
     return tweets
@@ -24,19 +23,5 @@ def get_data():
 
 if __name__=="__main__":
     tweets = get_data()
-    males, females = {}, {}
-    with open('./tweet_data/males.txt') as f:
-        males = set([w.strip() for w in f.readlines()])
-    with open('./tweet_data/females.txt') as f:
-        females = set([w.strip() for w in f.readlines()])
-
-    males_c, females_c, not_found = 0, 0, 0
-    for t in tweets:
-        if t['name'] in males:
-            males_c += 1
-        elif t['name'] in females:
-            females_c += 1
-        else:
-            not_found += 1
-    print males_c, females_c, not_found
+    
     pdb.set_trace()
