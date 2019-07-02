64a2a0a9c8951311aa79dd9dddaaf9d4ff45997b
theundisclosedlife <31027232+theundisclosedlife@users.noreply.github.com>
2019-07-02 14:07:13 +0530

    dumbo

--- data_handler.py
+++ data_handler.py
@@ -10,7 +10,7 @@ def get_data():
     df = df.drop(["Unnamed: 0"], axis=1)
     df['polarity'] = (df['Code']=="H").astype(int)
    
-    for i in range(0,len(df['Tweet']):
+    for i in range(0,len(df['Tweet'])):
         tweets.append({'text' : df['Tweet'][i].lower(),
                        'label': df['polarity'][i]
                       })
