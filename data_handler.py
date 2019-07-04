8c717c1440d0a76af79b41561dcb59ce15b8713c
theundisclosedlife <31027232+theundisclosedlife@users.noreply.github.com>
2019-07-04 15:10:08 +0530

    Update data_handler.py

--- data_handler.py
+++ data_handler.py
@@ -8,6 +8,42 @@ def get_data():
     tweets = []
     df=pd.read_csv("Untitled.csv")
     df = df.drop(["Unnamed: 0"], axis=1)
+    df = df.drop_duplicates(subset ="Tweet", 
+                     keep = False)
+    df=df.reset_index(drop=True)
+    
+    def clean_text(text):
+    
+   
+    
+        text = re.sub(r"\A[A-Za-z][^ ]*", "", text)  
+
+        text = re.sub(r"[^A-Za-z0-9^,!.\/'+-=]", " ", text)
+        text = re.sub(r"what's", "what is ", text)
+        text = re.sub(r"\'s", " ", text)
+        text = re.sub(r"\'ve", " have ", text)
+        text = re.sub(r"n't", " not ", text)
+        text = re.sub(r"i'm", "i am ", text)
+        text = re.sub(r"\'re", " are ", text)
+        text = re.sub(r"\'d", " would ", text)
+        text = re.sub(r"\'ll", " will ", text)
+        text = re.sub(r",", " ", text)
+        text = re.sub(r"\.", " ", text)
+        text = re.sub(r"!", " ! ", text)
+        text = re.sub(r"\/", " ", text)
+        text = re.sub(r"\^", " ^ ", text)
+        text = re.sub(r"\+", " + ", text)
+        text = re.sub(r"\-", " - ", text)
+        text = re.sub(r"\=", " = ", text)
+        text = re.sub(r"'", " ", text)
+
+    
+    return text
+# apply the above function to df['text']
+    df['Tweet'] = df['Tweet'].map(lambda x: clean_text(x))
+    
+    
+    
     df['polarity'] = (df['Code']=="H").astype(int)
    
     for i in range(0,len(df['Tweet'])):
